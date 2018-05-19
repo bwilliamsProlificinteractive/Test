18cf9c275f6a84fab42eb7f1c36b93dae52b5344
Azhar Jamil <azhar@teqforcesolutions.com>
2018-05-19 15:10:05 +0530

    Hello once

--- index.php
+++ index.php
@@ -1,32 +1,38 @@
-    public List<Account> allAccs{get;set;}
-{
-        allAccs = new List<Account>();
-        Account acc1 = new Account();
-        allAccs.add(acc1);
-    }
-    public void addRowMethod(){
-        Account acc1 = new Account();
-        allAccs.add(acc1);
-    }
-        
-    public void removeRowMethod(){
-        integer count = allAccs.size();
-        allAccs.remove(count-1);
-    }
-    public void saveMethod(){
-        insert allAccs;
-    }
-    
-    public void clearMethod(){
-        integer finalCount = allAccs.size();
-        allAccs.clear();
-        for(integer i=0;i<finalCount;i++){
-            Account acc1 = new Account();
-            allAccs.add(acc1);
-            
-        }
-    
-    }
-    
-    
-}
+<!--  ----------------------------------------------------------------------  -->
+<!--  NOTE: Please add the following <META> element to your page <HEAD>.      -->
+<!--  If necessary, please modify the charset parameter to specify the        -->
+<!--  character set of your HTML page.                                        -->
+<!--  ----------------------------------------------------------------------  -->
+
+<META HTTP-EQUIV="Content-type" CONTENT="text/html; charset=UTF-8">
+
+<!--  ----------------------------------------------------------------------  -->
+<!--  NOTE: Please add the following <FORM> element to your page.             -->
+<!--  ----------------------------------------------------------------------  -->
+
+<form action="https://webto.salesforce.com/servlet/servlet.WebToLead?encoding=UTF-8" method="POST">
+
+<input type=hidden name="oid" value="00D1I000003nSqw">
+<input type=hidden name="retURL" value="http://">
+
+<!--  ----------------------------------------------------------------------  -->
+<!--  NOTE: These fields are optional debugging elements. Please uncomment    -->
+<!--  these lines if you wish to test in debug mode.                          -->
+<!--  <input type="hidden" name="debug" value=1>                              -->
+<!--  <input type="hidden" name="debugEmail"                                  -->
+<!--  value="bwilliams@prolificinteractive.com">                              -->
+<!--  ----------------------------------------------------------------------  -->
+
+<label for="first_name">First Name</label><input  id="first_name" maxlength="40" name="first_name" size="20" type="text" /><br>
+
+<label for="last_name">Last Name</label><input  id="last_name" maxlength="80" name="last_name" size="20" type="text" /><br>
+
+<label for="email">Email</label><input  id="email" maxlength="80" name="email" size="20" type="text" /><br>
+
+<label for="company">Company</label><input  id="company" maxlength="40" name="company" size="20" type="text" /><br>
+
+<label for="description">Description</label><textarea name="description"></textarea><br>
+
+<input type="submit" name="submit">
+
+</form>
