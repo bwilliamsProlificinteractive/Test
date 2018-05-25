d7a31de963331ba2d0602af84e3ec9990438b384
Azhar Jamil <azhar@teqforcesolutions.com>
2018-05-25 14:25:16 +0530

    Label Update Today

--- index.php
+++ index.php
@@ -1,20 +1,21 @@
 <?php
-?>
-<!--  ----------------------------------------------------------------------  -->
+?><!--  ----------------------------------------------------------------------  -->
 <!--  NOTE: Please add the following <META> element to your page <HEAD>.      -->
 <!--  If necessary, please modify the charset parameter to specify the        -->
 <!--  character set of your HTML page.                                        -->
 <!--  ----------------------------------------------------------------------  -->
+
 <META HTTP-EQUIV="Content-type" CONTENT="text/html; charset=UTF-8">
+<link rel="stylesheet" type="text/css" href="copiedCSS.css"/>
 
 <!--  ----------------------------------------------------------------------  -->
 <!--  NOTE: Please add the following <FORM> element to your page.             -->
 <!--  ----------------------------------------------------------------------  -->
-<link rel="stylesheet" type="text/css" href="copiedCSS.css"/>
-<div class="connect-section"><form class="connect-form" action="https://webto.salesforce.com/servlet/servlet.WebToLead?encoding=UTF-8" method="POST">
+
+<form class="connect-form" action="https://webto.salesforce.com/servlet/servlet.WebToLead?encoding=UTF-8" method="POST">
 
 <input type=hidden name="oid" value="00D1I000003nSqw">
-<input type=hidden name="retURL" value="http://">
+<input type=hidden name="retURL" value="https://www.prolificinteractive.com/work/">
 
 <!--  ----------------------------------------------------------------------  -->
 <!--  NOTE: These fields are optional debugging elements. Please uncomment    -->
@@ -24,16 +25,20 @@
 <!--  value="bwilliams@prolificinteractive.com">                              -->
 <!--  ----------------------------------------------------------------------  -->
 <h2 class="connect-section-headline">Let's build something remarkable together.</h2>
-<label for="first_name">First Name</label><div class="input-field"> <input id="first_name" maxlength="40" placeholder="First Name" name="first_name" size=20 type="text" /><br></div>
+<label for="first_name">First Name</label><div class="input-field"><input  id="first_name" maxlength="40" name="first_name" size="20" type="text" /><br></div>
 
-<label for="last_name">Last Name</label> <div class="input-field"> <input  id="last_name" maxlength="80" placeholder="Last Name" name="last_name" size="20" type="text" /><br> </div>
+<label for="last_name">Last Name</label><div class="input-field"><input  id="last_name" maxlength="80" name="last_name" size="20" type="text" /><br></div>
 
-<label for="email">Email</label> <div class="input-field"> <input  id="email" maxlength="80" placeholder="Email" name="email" size="60" type="text" /></div><br>
+I'm interested in...<select  id="00N1I00000LqV18" name="00N1I00000LqV18" title="Connect Form - Interested in:"><option value=""><div class="input-field input-field-interested-in"> <div class="select-wrapper"><span class="caret">▼</span>
+	<input type="text" class="select-dropdown" readonly="true" value="New Business"><ul class="dropdown-content select-dropdown" style="width: 350px; position: absolute; top: 0px; left: 0px; opacity: 1; display: none;"><li class=""><span>New Business</span></li><li class=""><span>Career Opportunities</span></li><li class="active selected"><span>Press</span></li><li class=""><span>Other</span></li></ul><select id="interest" name="interest" class="initialized"><option value="New Business">New Business</option><option value="Career Opportunities">Career Opportunities</option><option value="Press">Press</option><option value="Other">Other</option> </select></div></div>
+</select><br>
 
-<label for="company">Company</label><div class="input-field"><input  id="company" maxlength="40" placeholder="Company" name="company" size="20" type="text" /></div><br>
+<label for="company">Company</label><div class="input-field"><input  id="company" maxlength="40" name="company" size="20" type="text" /><br></div>
 
-<label>Description</label><div class="input-field input-field-message"><textarea id="description" placeholder="Description" name="message" class="materialize-textarea invalid" required="" data-parsley-id="15"></textarea></div>
+<label for="email">Email</label><div class="input-field"><input  id="email" maxlength="80" name="email" size="20" type="text" /><br></div>
 
-<button type="submit" class="btn btn-primary form-submit">Submit</button>
+<label>What problem can we help you solve? Tell us about your project.</label><div class="input-field "><textarea  id="00N1I00000LqV1D" name="00N1I00000LqV1D" class="materialize-textarea"></textarea><br></div>
 
-</form></div>
+<input type="submit" class="btn btn-primary form-submit" name="submit">
+
+</form>
