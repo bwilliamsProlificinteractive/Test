41484f57cc551df5622a1a54da4117fda29e7999
Azhar Jamil <azhar@teqforcesolutions.com>
2018-06-01 11:46:32 +0530

    Today commit 1st June

--- index.php
+++ index.php
@@ -1,44 +1,53 @@
 <?php
-?><!--  ----------------------------------------------------------------------  -->
-<!--  NOTE: Please add the following <META> element to your page <HEAD>.      -->
-<!--  If necessary, please modify the charset parameter to specify the        -->
-<!--  character set of your HTML page.                                        -->
-<!--  ----------------------------------------------------------------------  -->
+?><!doctype html>
+<html>
 
-<META HTTP-EQUIV="Content-type" CONTENT="text/html; charset=UTF-8">
-<link rel="stylesheet" type="text/css" href="copiedCSS.css"/>
+    <link rel='preload' id='pi-css-css' href='https://www.prolificinteractive.com/wp-content/themes/prolific-interactive/dist/styles/main-9762162602.css' onload="this.rel='stylesheet'" as='style' />
 
-<!--  ----------------------------------------------------------------------  -->
-<!--  NOTE: Please add the following <FORM> element to your page.             -->
-<!--  ----------------------------------------------------------------------  -->
+<body class="page-template page-template-templates page-template-connect page-template-templatesconnect-php page page-id-187 connect">
+    
+                <div class="connect-section">
+                    <h2 class="connect-section-headline">Let's build something remarkable together.</h2>
+                    <div class="connect-form-wrapper">
+                        <form class="connect-form" action="https://webto.salesforce.com/servlet/servlet.WebToLead?encoding=UTF-8" method="POST">
+                           <input type=hidden name="oid" value="00D1I000003nSqw">
+<input type=hidden name="retURL" value="https://www.prolificinteractive.com/work/"> 
+                            <div>
+                                <div class="input-field">
+                                    <input id="name1" placeholder="Jane Smith" type="text" name="name" required>
+                                    <label for="name" data-error="Required">Name</label>
+                                </div>
+                                <div class="input-field input-field-interested-in">
+                                    <select id="interest" name="interest1">
+                                        <option value="New Business">New Business</option>
+                                        <option value="Career Opportunities">Career Opportunities</option>
+                                        <option value="Press">Press</option>
+                                        <option value="Other">Other</option>
+                                    </select>
+                                    <label for="interest" data-error="Required">I'm interested in...</label>
+                                </div>
+                                <div class="input-field">
+                                    <input id="company1" placeholder="Dream Partner" type="text" name="company" data-parsley-validate-if-empty="true" data-parsley-required-if-interest="New Business">
+                                    <label for="company" data-error="Required">Company</label>
+                                </div>
+                                <div class="input-field">
+                                    <input id="email1" placeholder="janesmith@dreampartner.com" type="email" name="email" required>
+                                    <label for="email" data-error="Invalid Email">Email</label>
+                                </div>
+                                <div class="input-field input-field-message">
+                                    <textarea id="message1" placeholder="Hi, Prolific!" name="message" class="materialize-textarea" required></textarea>
+                                    <label for="message" data-error="Required">What problem can we help you solve?
+                                        <br /> Tell us about your project.</label>
+                                </div>
+                            </div>
+                            <input type="submit" class="btn btn-primary" name="submit">
+                        </form>
+    
+    <script>
+        (window.jQuery && jQuery.noConflict()) || document.write('<script src="https://www.prolificinteractive.com/wp-includes/js/jquery/jquery.js"><\/script>')
+    </script>
+    
+    <script type="text/javascript" defer src="https://www.prolificinteractive.com/wp-content/cache/autoptimize/js/autoptimize_0e28e7d0de9a7a8d4f11431d5133a982.js"></script>
+</body>
 
-<form class="connect-form" action="https://webto.salesforce.com/servlet/servlet.WebToLead?encoding=UTF-8" method="POST">
-
-<input type=hidden name="oid" value="00D1I000003nSqw">
-<input type=hidden name="retURL" value="https://www.prolificinteractive.com/work/">
-
-<!--  ----------------------------------------------------------------------  -->
-<!--  NOTE: These fields are optional debugging elements. Please uncomment    -->
-<!--  these lines if you wish to test in debug mode.                          -->
-<!--  <input type="hidden" name="debug" value=1>                              -->
-<!--  <input type="hidden" name="debugEmail"                                  -->
-<!--  value="bwilliams@prolificinteractive.com">                              -->
-<!--  ----------------------------------------------------------------------  -->
-<h2 class="connect-section-headline">Let's build something remarkable together.</h2>
-<label for="first_name">First Name</label><div class="input-field"><input  id="first_name" maxlength="40" name="first_name" size="20" type="text" /><br></div>
-
-<label for="last_name">Last Name</label><div class="input-field"><input  id="last_name" maxlength="80" name="last_name" size="20" type="text" /><br></div>
-
-I'm interested in...<select  id="00N1I00000LqV18" name="00N1I00000LqV18" title="Connect Form - Interested in:"><option value=""><div class="input-field input-field-interested-in"> <div class="select-wrapper"><span class="caret">▼</span>
-	<input type="text" class="select-dropdown" readonly="true" value="New Business"><ul class="dropdown-content select-dropdown" style="width: 350px; position: absolute; top: 0px; left: 0px; opacity: 1; display: none;"><li class=""><span>New Business</span></li><li class=""><span>Career Opportunities</span></li><li class="active selected"><span>Press</span></li><li class=""><span>Other</span></li></ul><select id="interest" name="interest" class="initialized"><option value="New Business">New Business</option><option value="Career Opportunities">Career Opportunities</option><option value="Press">Press</option><option value="Other">Other</option> </select></div></div>
-</select><br>
-
-<label for="company">Company</label><div class="input-field"><input  id="company" maxlength="40" name="company" size="20" type="text" /><br></div>
-
-<label for="email">Email</label><div class="input-field"><input  id="email" maxlength="80" name="email" size="20" type="text" /><br></div>
-
-<label>What problem can we help you solve? Tell us about your project.</label><div class="input-field "><textarea  id="00N1I00000LqV1D" name="00N1I00000LqV1D" class="materialize-textarea"></textarea><br></div>
-
-<input type="submit" class="btn btn-primary form-submit" name="submit">
-
-</form>
+</html>
