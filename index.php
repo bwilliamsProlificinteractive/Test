d5527bec4a43814048e22446c9a82fa1a979d441
Azhar Jamil <azhar@teqforcesolutions.com>
2018-06-01 12:41:59 +0530

    Testing

--- index.php
+++ index.php
@@ -11,14 +11,18 @@
                     <div class="connect-form-wrapper">
                         <form class="connect-form" action="https://webto.salesforce.com/servlet/servlet.WebToLead?encoding=UTF-8" method="POST">
                            <input type=hidden name="oid" value="00D1I000003nSqw">
-<input type=hidden name="retURL" value="https://www.prolificinteractive.com/work/"> 
+                            <input type=hidden name="retURL" value="https://www.prolificinteractive.com/work/"> 
                             <div>
                                 <div class="input-field">
-                                    <input id="name1" placeholder="Jane Smith" type="text" name="name" required>
-                                    <label for="name" data-error="Required">Name</label>
+                                    <input id="first_name" placeholder="Jane" type="text" name="name" required>
+                                    <label for="name" data-error="Required">First Name</label>
+                                </div>
+                                <div class="input-field">
+                                    <input id="last_name" placeholder="Smith" type="text" name="name" required>
+                                    <label for="name" data-error="Required">Last Name</label>
                                 </div>
                                 <div class="input-field input-field-interested-in">
-                                    <select id="interest" name="interest1">
+                                    <select id="interest" name="interest">
                                         <option value="New Business">New Business</option>
                                         <option value="Career Opportunities">Career Opportunities</option>
                                         <option value="Press">Press</option>
@@ -27,19 +31,19 @@
                                     <label for="interest" data-error="Required">I'm interested in...</label>
                                 </div>
                                 <div class="input-field">
-                                    <input id="company1" placeholder="Dream Partner" type="text" name="company" data-parsley-validate-if-empty="true" data-parsley-required-if-interest="New Business">
+                                    <input id="company" placeholder="Dream Partner" type="text" name="company" data-parsley-validate-if-empty="true" data-parsley-required-if-interest="New Business">
                                     <label for="company" data-error="Required">Company</label>
                                 </div>
                                 <div class="input-field">
-                                    <input id="email1" placeholder="janesmith@dreampartner.com" type="email" name="email" required>
+                                    <input id="email" placeholder="janesmith@dreampartner.com" type="email" name="email" required>
                                     <label for="email" data-error="Invalid Email">Email</label>
                                 </div>
                                 <div class="input-field input-field-message">
-                                    <textarea id="message1" placeholder="Hi, Prolific!" name="message" class="materialize-textarea" required></textarea>
+                                    <textarea id="00N1I00000LqV1D" placeholder="Hi, Prolific!" name="message" class="materialize-textarea" required></textarea>
                                     <label for="message" data-error="Required">What problem can we help you solve?
                                         <br /> Tell us about your project.</label>
                                 </div>
-                            </div>
+                            </div><br>
                             <input type="submit" class="btn btn-primary" name="submit">
                         </form>
     
