a6bac0f6b10a950433b13c82c056cd8847a85ece
Azhar Jamil <azhar@teqforcesolutions.com>
2018-06-02 11:56:57 +0530

    Test

--- index.php
+++ index.php
@@ -4,7 +4,7 @@
 
     <link rel='preload' id='pi-css-css' href='https://www.prolificinteractive.com/wp-content/themes/prolific-interactive/dist/styles/main-9762162602.css' onload="this.rel='stylesheet'" as='style' />
 
-<body scroll="no" class="page-template page-template-templates page-template-connect page-template-templatesconnect-php page page-id-187 connect">
+<body scroll="no">
     
                 <div class="connect-section">
                     <h2 class="connect-section-headline">Let's build something remarkable together.</h2>
